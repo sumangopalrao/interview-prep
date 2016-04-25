7e57bbf99cdc37bae2021ebb6baa153a93e8f12d
austin665 <suman.austin@gmail.com>
2016-04-24 19:06:36 -0700

    Sum and smallest

---
+++ src/arrays/SmallestandSmaller.java
@@ -0,0 +1,27 @@
+package arrays;
+
+public class SmallestandSmaller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = { 6, 4, 3, 1, 5};
+		int lowest=a[0];
+		int lower=a[0];
+		for(int i=1; i<a.length; i++)
+		{
+			if(a[i] < lowest)
+			{
+				lower = lowest;
+				lowest = a[i];
+			}
+			else if(a[i] < lower)
+			{
+				lower = a[i];
+			}
+		}
+		System.out.println("Lowest:" + lowest);
+		System.out.println("Second Lowest:" + lower);
+
+	}
+
+}
---
+++ src/arrays/SumCloseToZero.java
@@ -0,0 +1,36 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class SumCloseToZero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {-4, 4, 1, 2, -5, 1};
+		
+		Arrays.sort(a);
+		
+		int sum;
+		int min_sum, min_l, min_r;
+		int l=min_l=0;
+		int r=min_r=a.length-1;
+        min_sum=Integer.MAX_VALUE;
+		while (l<r)
+		{
+			sum = a[l] + a[r];
+			if(Math.abs(sum) < Math.abs(min_sum))
+			{
+				min_sum = sum;
+				min_l = l;
+				min_r = r;
+			}
+			else if(sum > 0)
+				r--;
+			else 
+				l++;
+		}
+		System.out.println("Left" + a[min_l] + "Right:" + a[min_r]);
+
+	}
+
+}
