fd33f1e7b7ec7725f8d0fdc2f17c09f07678b69a
austin665 <suman.austin@gmail.com>
2016-05-08 22:37:06 -0700

    Detect Loop

---
+++ src/linkedlist/DetectLoop.java
@@ -0,0 +1,33 @@
+package linkedlist;
+
+public class DetectLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node a = new Node(1);
+		Node b = new Node(2);
+		Node c = new Node(3);
+		Node d = new Node(4);
+		Node e = new Node(5);
+		a.link = b; b.link = c; c.link=d; d.link = e; e.link = null;
+		System.out.println(checkLoop(a));
+
+	}
+	
+	static Boolean checkLoop(Node head)
+	{
+		Node slower = head;
+		Node runner = head.link;
+		
+		while(slower != null && runner != null && runner.link != null)
+		{
+			if(slower == runner)
+				return true;
+			slower = slower.link;
+		    runner = runner.link.link;
+		}
+		return false;
+		
+	}
+
+}
