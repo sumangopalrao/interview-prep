79802fbca2750a69d3d5166f12b83158652c8c09
austin665 <suman.austin@gmail.com>
2016-05-08 22:04:24 -0700

    Merge linked list

---
+++ src/linkedlist/Merge.java
@@ -0,0 +1,59 @@
+package linkedlist;
+
+public class Merge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Node currA = Node.initializeSample();
+		Node currB = Node.initializeSampleB();
+		Node c = merge(currA, currB);
+	    Node.printList(c);	
+	}
+	
+	static Node merge(Node headA, Node headB)
+	{
+		if(headA== null || headB == null)
+			return null;
+		
+		Node currA = headA;
+		Node currB= headB;
+		Node currC = new Node();
+		Node headC = currC;
+		
+		while(currA != null && currB != null)
+		{
+		    if(currA.data <= currB.data)
+		    {
+		    	currC.link = currA;
+		    	currA = currA.link;
+		    }
+		    else if(currA.data >= currB.data)
+		    {
+		    	currC.link = currB;
+		    	currB = currB.link;
+		    }
+		    currC = currC.link;
+		}
+		
+		while(currA != null)
+		{
+			currC.link = currA;
+			currC = currC.link;
+			currA = currA.link;
+		}
+		
+		while(currB != null)
+		{
+			currC.link = currB;
+			currC = currC.link;
+			currB = currB.link;
+		}
+		
+		return headC.link;
+		
+		
+		
+	}
+
+}
--- src/linkedlist/Node.java
+++ src/linkedlist/Node.java
@@ -4,6 +4,11 @@ public class Node {
 	int data;
 	Node link;
 	
+	public Node ()
+	{
+		this.link = null;
+				
+	}
 	public Node(int data)
 	{
 		this.data = data;
@@ -12,6 +17,8 @@ public class Node {
 	
 	public static void printList(Node head)
 	{
+		if(head == null)
+		    System.out.println("null");
 		while(head != null)
 		{
 			System.out.print(head.data + "--->");
@@ -21,16 +28,33 @@ public class Node {
 	
 	public static Node initializeSample()
 	{
-		Node a = new Node(5);
+		Node a = new Node(2);
 		Node b = new Node(4);
-		Node c = new Node(2);
-		Node d = new Node(6);
-		Node e = new Node(4);
+		Node c = new Node(6);
+		Node d = new Node(8);
+		Node e = new Node(10);
 		a.link = b; b.link = c; c.link=d; d.link = e; e.link = null;
 	    Node start= a;	
 	    while (start!= null)
 	    {
-	    	System.out.print(start.data + "---->");
+//	    	System.out.print(start.data + "---->");
+	    	start = start.link;
+	    }
+	    return a;
+	}
+	
+	public static Node initializeSampleB()
+	{
+		Node a = new Node(1);
+		Node b = new Node(2);
+		Node c = new Node(3);
+		Node d = new Node(4);
+		Node e = new Node(5);
+		a.link = b; b.link = c; c.link=d; d.link = e; e.link = null;
+	    Node start= a;	
+	    while (start!= null)
+	    {
+//	    	System.out.print(start.data + "---->");
 	    	start = start.link;
 	    }
 	    return a;
