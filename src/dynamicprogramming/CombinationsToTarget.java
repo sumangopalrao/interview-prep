a8ab3674aad5c15143d784c2fd74dc2789825440
Yes Man <suman.gopalrao@gmail.com>
2017-05-22 23:13:58 -0700

    DP Num of ways

---
+++ src/dynamicprogramming/CombinationsToTarget.java
@@ -0,0 +1,18 @@
+//Program to find the number ways to reach a given target given scores such as 2, 3 and 7.
+//C18.1 EPI
+package dynamicprogramming;
+
+public class CombinationsToTarget {
+
+	private static int numOfWays(int n) {
+		if(n < 2)
+			return 0;
+		return 1+numOfWays(n-2)+numOfWays(n-3)+numOfWays(n-7);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(numOfWays(4));
+	}
+
+}
