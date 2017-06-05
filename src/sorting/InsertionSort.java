d5ce721737c7f2d1678b8d29f645df3a39b9e78c
Yes Man <suman.gopalrao@gmail.com>
2017-06-04 21:26:34 -0700

    Insertion Sort

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,28 @@
+package sorting;
+
+public class InsertionSort {
+
+	public static int[] sort(int[] arr) {
+		for(int i=0; i<arr.length; i++) {
+			int val = arr[i];
+			for(int j=0; j<i; j++) {
+				if(val <  arr[j]) {
+					System.arraycopy(arr, j, arr, j+1, i-j);
+					arr[j] = val;
+					break;
+				}
+			}
+		}
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = new int[]{3, 4, 2, 1, 5, -1, -2, -100, -34, -22};
+		int[] out = sort(arr);
+		for(Integer i: out) {
+			System.out.println(i+",");
+		}
+	}
+
+}
