9ffd5e1d7f06551d7fdb247e5e5cde7ce1bc286f
Yes Man <suman.gopalrao@gmail.com>
2017-06-07 21:23:34 -0700

    In order traversal.

---
+++ src/binarytree/InOrderTraversal.java
@@ -0,0 +1,27 @@
+//In order traversal of binary tree.
+package binarytree;
+
+public class InOrderTraversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Node root = new Node(1);
+        root.left  = new Node(2);
+        root.right = new Node(3);
+        root.left.right= new Node(4);
+        //root.left.right.right= new Node(5);
+        
+        inorder(root);
+
+	}
+	
+	private static void inorder(Node root) {
+		if(root == null)
+			return;
+		inorder(root.left);
+		System.out.println(root.data+",");
+		inorder(root.right);
+	}
+
+}
