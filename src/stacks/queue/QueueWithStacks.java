1ea980cc36a1654e931e5d608d7cdf85ca018c02
Yes Man <suman.gopalrao@gmail.com>
2017-08-02 15:22:46 -0700

    Implement a Q using stacks.

---
+++ src/stacks/queue/QueueWithStacks.java
@@ -0,0 +1,65 @@
+//Program to implement a myqueue class using stacks.
+//CTCI 3.5
+
+package stacks.queue;
+
+public class QueueWithStacks {
+	
+	Stack<Integer> stA;
+	Stack<Integer> stB;
+	
+	QueueWithStacks() {
+		stA = new Stack<Integer>();
+		stB = new Stack<Integer>();
+	}
+	
+	public void offer(int val) {
+		stA.push(val);
+	}
+	
+	public int peek() {
+		if(!stB.isEmpty())
+			return stB.peek();
+		else {
+			while(!stA.isEmpty()) {
+				stB.push(stA.pop());
+			}
+		}
+		return stB.peek();
+	}
+	
+	public void poll() {
+		if(!stB.isEmpty())
+			stB.pop();
+		else {
+			while(!stA.isEmpty()) {
+				stB.push(stA.pop());
+			}
+		}
+	}
+	
+	public boolean isEmpty() {
+		if(stA.isEmpty() && stB.isEmpty())
+			return true;
+		return false;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		QueueWithStacks q = new QueueWithStacks();
+		
+		q.offer(1);
+		q.offer(4);
+		q.offer(3);
+		q.offer(2);
+		System.out.println(q.peek());
+		q.poll();
+		q.poll();
+		System.out.println(q.peek());
+
+	}
+
+}
