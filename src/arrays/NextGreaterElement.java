52230b835ad9896fc05f96582cd379c090b81bb7
Suman Rao <suman.gopalrao@gmail.com>
2022-04-21 23:47:04 +0530

    Updated next greater element

--- src/arrays/NextGreaterElement.java
+++ src/arrays/NextGreaterElement.java
@@ -4,9 +4,7 @@ package arrays;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Stack;
 
 public class NextGreaterElement {
