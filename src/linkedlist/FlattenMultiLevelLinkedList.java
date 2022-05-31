4a26dd1dd7b16f24b6a908500bfcf09b65a3ff1e
Suman Rao <suman.gopalrao@gmail.com>
2022-05-31 23:43:43 +0530

    Fix doubly linked list class

--- src/linkedlist/FlattenMultiLevelLinkedList.java
+++ src/linkedlist/FlattenMultiLevelLinkedList.java
@@ -3,25 +3,24 @@
 public class FlattenMultiLevelLinkedList {
     DoublyLinkedListNodeWithChild tail = null;
     public DoublyLinkedListNodeWithChild flatten(DoublyLinkedListNodeWithChild head) {
-    if(head == null) return null;
-    
-    head.prev = tail;
-    tail = head;
-    
-    DoublyLinkedListNodeWithChild nextNode = head.next;
-    
-    head.next = flatten(head.child);
-    head.child = null;
-    
-    tail.next = flatten(nextNode);
-    
-    return head;
+        if(head == null) return null;
+        
+        head.prev = tail;
+        tail = head;
+        
+        DoublyLinkedListNodeWithChild nextNode = head.next;
+        
+        head.next = flatten(head.child);
+        head.child = null;
+        
+        tail.next = flatten(nextNode);
+        
+        return head;
     }
-}
-class DoublyLinkedListNodeWithChild {
-    public int val;
-    public DoublyLinkedListNodeWithChild prev;
-    public DoublyLinkedListNodeWithChild next;
-    public DoublyLinkedListNodeWithChild child;
-};
+    class DoublyLinkedListNodeWithChild {
+        public int val;
+        public DoublyLinkedListNodeWithChild prev;
+        public DoublyLinkedListNodeWithChild next;
+        public DoublyLinkedListNodeWithChild child;
+    };
 }
