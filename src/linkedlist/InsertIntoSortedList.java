0f5e345faea5e0c3505fecb0412b3a996d2781e5
Suman Rao <suman.gopalrao@gmail.com>
2022-06-06 23:47:42 +0530

    Insert into sorted list

--- src/linkedlist/InsertIntoSortedList.java
+++ src/linkedlist/InsertIntoSortedList.java
@@ -2,19 +2,19 @@
 package linkedlist;
 
 public class InsertIntoSortedList {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Node a = new Node(1);
-		Node b = new Node(2);
-		Node c = new Node(3);
-		Node d = new Node(4);
-		Node e = new Node(5);
-		a.link = b; b.link = c; c.link=d; d.link = e; e.link = a;
-		System.out.println(insert(a, 10));
-
-	}
-
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        a.link = b; b.link = c; c.link=d; d.link = e; e.link = a;
+        System.out.println(insert(a, 10));
+        
+    }
+    
     static public Node insert(Node head, int insertVal) {
         if(head == null) {
             Node newNode = new Node(insertVal);
@@ -40,6 +40,5 @@ public class InsertIntoSortedList {
         node.link = new Node(insertVal);
         node.link.link = next;
         return head;
-        }
-
+    }
 }
