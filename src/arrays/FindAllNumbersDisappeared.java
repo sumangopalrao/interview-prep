e96b927505b7e5b5c8802f7420ea4393df5a2d04
Suman Rao <suman.gopalrao@gmail.com>
2022-06-24 21:28:21 +0530

    Fix find all numbers disappeared

--- src/arrays/FindAllNumbersDisappeared.java
+++ src/arrays/FindAllNumbersDisappeared.java
@@ -25,11 +25,6 @@ public class FindAllNumbersDisappeared {
 
     public static List<Integer> findDisappearedNumbersOnePass(int[] nums) {
 
-        // Set<Integer> allNumbers = new HashSet<Integer>();
-        // for(int i : nums) {
-            // allNumbers.add(i);
-        // }
-
         List<Integer> res = new ArrayList<Integer>();
 
         int i = 0;
@@ -48,23 +43,12 @@ public class FindAllNumbersDisappeared {
             System.out.println(val); 
         }
 
-
-        // for(int i=1; i<=nums.length; i++) {
-        //     if(!allNumbers.contains(i)) {
-        //         res.add(i);
-        //     }
-        // }
         return res;
 
     }
 
     public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// ArrayList<Integer> inList = new ArrayList<Integer>(Arrays.asList(4,3,2,1));
-		// List<Integer> res = nextGreater(inList);
-		// for(Integer i : res) {
-		// 	System.out.println(i);
-		// }
         int[] arr = {1, 3, 2, 1};
         List<Integer> res = findDisappearedNumbersOnePass(arr);
         for(Integer i : res) {
