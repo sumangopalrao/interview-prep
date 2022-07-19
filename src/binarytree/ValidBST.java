327532dd8b758411c638159f45086a45de8c33ee
Suman Rao <suman.gopalrao@gmail.com>
2022-07-19 10:57:26 +0530

    Add valid BST problem

--- src/binarytree/ValidBST.java
+++ src/binarytree/ValidBST.java
@@ -1,19 +1,20 @@
-//Program to check if BST is valid
+//Leetcode: Program to check if BST is valid
 package binarytree;
 
 public class ValidBST {
 
-	static int lower = Integer.MIN_VALUE;
-	static int higher = Integer.MAX_VALUE;
-	
 	static boolean isValidBST(Node root) {
-		return isValidHelper(root, lower, higher);
+		if (root == null)
+			return false;
+		if (root.left == null && root.right == null)
+			return true;
+		return isValidHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
 	}
 	
-	static boolean isValidHelper(Node root, int min, int max) {
+	static boolean isValidHelper(Node root, long min, long max) {
 		if(root == null)
 			return true;
-		if(root.data < min || root.data > max) {
+		if(root.data <= min || root.data >= max) {
 			return false;
 		}
 		return isValidHelper(root.left, min, root.data) && isValidHelper(root.right, root.data, max);
@@ -21,10 +22,10 @@ public class ValidBST {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Node node = new Node(1);
+		Node node = new Node(2);
 		node.left = new Node(2);
-		node.right = new Node(3);
+		node.right = new Node(2);
 		System.out.println(isValidBST(node));
+		System.out.println(isValidBST(new Node(2147483647)));
 	}
-
 }
