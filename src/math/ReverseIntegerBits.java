e87bef2b4c3b33d5f2fe623aa5d37bba0c860ba4
Suman Rao <suman.gopalrao@gmail.com>
2022-08-08 21:50:53 +0530

    Fixed reverse integer bits

--- src/math/ReverseIntegerBits.java
+++ src/math/ReverseIntegerBits.java
@@ -5,17 +5,21 @@ public class ReverseIntegerBits {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int n = 43261596;
 		int n = 4;
+		int r = reverseBits(n);
+		System.out.println(r);
+	}
+
+	private static int reverseBits(int n) {
 		int result = 0;
-	    for (int i = 0; i < 32; i++) {
-	    	int maskedVal = n&1;
-	        result += maskedVal;
-	        n >>>= 1;   // CATCH: must do unsigned shift
-	        if (i < 31) // CATCH: for last digit, don't shift!
-	            result <<= 1;
-	    }
-	    System.out.println(result);
+		for (int i = 0; i < 32; i++) {
+			int maskedVal = n &1;
+			result += maskedVal;
+			n >>>= 1;   // CATCH: must do unsigned shift
+			if (i < 31) // CATCH: for last digit, don't shift!
+				result <<= 1;
+		}
+		return result;
 	}
 
 }
