731f42cf3b8cf39eb4bcae9e7f28537dd197db6a
Suman Rao <suman.gopalrao@gmail.com>
2022-08-14 22:43:17 +0530

    Add valid parentheses problem

---
+++ src/ValidParentheses.java
@@ -0,0 +1,35 @@
+//Leetcode: Check if string has valid matching parentheses such that two parens are of the same type and open brackets are closed in the correct order
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        System.out.println(isValidParentheses("()"));
+        System.out.println(isValidParentheses("()[]{}"));
+        System.out.println(isValidParentheses("(]"));
+    }
+
+    private static boolean isValidParentheses(String str) {
+        Stack<Character> st = new Stack<>();
+
+        for (Character c : str.toCharArray()) {
+            if (c == '}') {
+                if (st.empty() || st.pop() != '{')
+                    return false;
+            }
+            else if( c == ')') {
+                if (st.empty() || st.pop() != '(')
+                    return false;
+            }
+            else if (c == ']') {
+                if (st.empty() || st.pop() != '[')
+                    return false;
+            } else {
+                st.push(c);
+            }
+        }
+
+        return st.empty();
+    }
+}
