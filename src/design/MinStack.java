df5badb7759064c580e558d7bc38cd2449d2eee6
Suman Rao <suman.gopalrao@gmail.com>
2022-08-31 23:35:54 +0530

    Learn Minstack problem

--- src/design/MinStack.java
+++ src/design/MinStack.java
@@ -6,63 +6,63 @@ import java.util.Stack;
 
 public class MinStack {
 
-        Stack<Tuple> stack;
-        // int minSoFar = Integer.MAX_VALUE;
+    Stack<Tuple> stack;
 
-        public MinStack() {
-            stack = new Stack<Tuple>();
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        int min;
+        if(stack.isEmpty() || stack.peek().min > val) {
+            min = val;
+        } else {
+            min = stack.peek().getMin();
+        }
+        stack.add(new Tuple(val, min));
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek().getVal();
+    }
+
+    public int getMin() {
+        Tuple t = stack.peek();
+        return t.getMin();
+    }
+
+    private static class Tuple {
+        int val;
+        int min;
+
+        Tuple(int val, int min) {
+            this.val = val;
+            this.min = min;
         }
 
-        public void push(int val) {
-            int min = 0;
-            if(stack.isEmpty() || stack.peek().min > val) {
-                min = val;
-            }
-            else
-            {
-                min = stack.peek().getMin();
-            }
-            stack.add(new Tuple(val, min));
+        int getMin() {
+            return min;
         }
 
-        public void pop() {
-            stack.pop();
-
-        }
-
-        public int top() {
-            return stack.peek().getVal();
-        }
-
-        public int getMin() {
-            Tuple t = stack.peek();
-            return t.getMin();
-        }
-
-        private class Tuple {
-            int val;
-            int min;
-
-            Tuple(int val, int min) {
-                this.val = val;
-                this.min = min;
-            }
-
-            int getMin() {
-                return min;
-            }
-
-            int getVal() {
-                return val;
-            }
+        int getVal() {
+            return val;
         }
     }
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
+    public static void main(String[] args) {
+        MinStack st = new MinStack();
+        st.push(2);
+        st.push(4);
+        st.push(-1);
+        st.push(3);
+        System.out.println(st.top());
+        System.out.println(st.getMin());
+        st.pop();
+        st.pop();
+        System.out.println(st.getMin());
+    }
+}
