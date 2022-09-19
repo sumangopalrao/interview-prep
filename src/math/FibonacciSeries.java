a0e0ff8c82d22d7b8d508657bce024c12f2ffe80
Suman Rao <suman.gopalrao@gmail.com>
2022-09-19 22:56:34 +0530

    Added fibonacci recursion

--- src/math/FibonacciSeries.java
+++ src/math/FibonacciSeries.java
@@ -16,13 +16,24 @@ public class FibonacciSeries {
 			a = b;
 			b = c;
 		}
-	}	
+	}
+
+	public static int fib(int n) {
+		if (n == 0)
+			return 0;
+
+		if (n == 1)
+			return 1;
+
+		return fib(n-1) + fib(n-2);
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		int n = 5;
 		printFib(n);
+		System.out.println(fib(5));
 		
 
 	}
