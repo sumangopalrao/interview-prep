d52ea1c61eec56df7c299380f4b560a74934884e
Suman Rao <suman.gopalrao@gmail.com>
2022-09-25 18:40:10 +0530

    Root equals sum of children

---
+++ src/binarytree/RootEqualsSumOfChildren.java
@@ -0,0 +1,33 @@
+// Leetcode: Check if the value of a root equals the sum of its children
+package binarytree;
+
+public class RootEqualsSumOfChildren {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Node root = new Node(10);
+        Node a = new Node(5);
+        Node b = new Node(6);
+//        Node c = new Node(4);
+//        Node d = new Node(5);
+//        Node e = new Node(6);
+
+        root.left = a;
+        root.right = b;
+//        a.left = c;
+//        a.right = d;
+//        b.left = e;
+
+        System.out.println(checkTree(root));
+    }
+
+    private static boolean checkTree(Node root) {
+        if(root == null) {
+            return false;
+        }
+
+        if (root.left == null || root.right == null)
+            return false;
+
+        return root.data == root.left.data + root.right.data;
+    }
+}
