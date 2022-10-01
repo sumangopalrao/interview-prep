bdc84dea30a128002447830b8a4b76e83e282cc4
Suman Rao <suman.gopalrao@gmail.com>
2022-10-01 23:54:31 +0530

    Working on find common chracters problem

---
+++ src/arrays/FindCommonCharacters.java
@@ -0,0 +1,25 @@
+package arrays;
+
+
+import java.util.List;
+
+public class FindCommonCharacters {
+
+    public static void main(String[] args) {
+        String[] words = {"bella","label","roller"};
+        List<String> out = commonChars(words);
+        for (String s : out) {
+            System.out.println(s);
+        }
+    }
+
+    public static List<String> commonChars(String[] words) {
+
+        for (int i = 97; i<=122; i++) {
+            for (String word : words) {
+                if (word.indexOf((char) i))
+            }
+        }
+
+    }
+}
