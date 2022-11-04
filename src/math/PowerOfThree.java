c2d8ff0f2a8a9561dd8c344cec8c86be8790b850
Suman Rao <suman.gopalrao@gmail.com>
2022-11-04 23:38:41 +0530

    Simplify power of three

--- src/math/PowerOfThree.java
+++ src/math/PowerOfThree.java
@@ -4,11 +4,7 @@ public class PowerOfThree {
 
     public static void main(String[] args) {
         int n = 27;
-//        double x = Math.log((double)n / 3);
-//        if ((x * 10) % 10 == 0)
-//            System.out.println(true);
-//        else System.out.println(false);
-        boolean res =  n > 0 && (Math.pow(3, 19) % n) ==0;
+        boolean res = Math.pow(3, 19) % n == 0;
         System.out.println(res);
     }
 }
