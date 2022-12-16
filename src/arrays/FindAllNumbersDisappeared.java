1022957ff713095328cfc2c78a026e94df6380e7
Suman Rao <suman.gopalrao@gmail.com>
2022-12-16 23:49:53 +0530

    Update find disappeared numbers

--- src/arrays/FindAllNumbersDisappeared.java
+++ src/arrays/FindAllNumbersDisappeared.java
@@ -9,18 +9,17 @@ public class FindAllNumbersDisappeared {
     public static List<Integer> findDisappearedNumbers(int[] nums) {
 
         Set<Integer> allNumbers = new HashSet<Integer>();
-        for(int i : nums) {
+        for (int i : nums) {
             allNumbers.add(i);
         }
 
         List<Integer> res = new ArrayList<Integer>();
-        for(int i=1; i<=nums.length; i++) {
-            if(!allNumbers.contains(i)) {
+        for (int i = 1; i <= nums.length; i++) {
+            if (!allNumbers.contains(i)) {
                 res.add(i);
             }
         }
         return res;
-
     }
 
     public static List<Integer> findDisappearedNumbersOnePass(int[] nums) {
@@ -29,18 +28,18 @@ public class FindAllNumbersDisappeared {
 
         int i = 0;
         while (i < nums.length) {
-            if(nums[i] == -1) {
+            if (nums[i] == -1) {
                 i++;
                 continue;
             }
             int temp = nums[i];
             nums[i] = nums[temp - 1];
-            nums[temp-1] = -1;
+            nums[temp - 1] = -1;
             i++;
         }
 
-        for(int val : nums) {
-            System.out.println(val); 
+        for (int val : nums) {
+            System.out.println(val);
         }
 
         return res;
@@ -48,11 +47,11 @@ public class FindAllNumbersDisappeared {
     }
 
     public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        int[] arr = {1, 3, 2, 1};
+        // TODO Auto-generated method stub
+        int[] arr = { 1, 3, 2, 1 };
         List<Integer> res = findDisappearedNumbersOnePass(arr);
-        for(Integer i : res) {
-            System.out.println(findDisappearedNumbers(arr));
+        for (Integer i : res) {
+            System.out.println(i);
         }
-	}
+    }
 }
