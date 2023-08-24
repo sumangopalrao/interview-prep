e0e92d74fa8c0a9049cc3daf18f5c762ea9d4caf
Suman Gopal Rao <suman.gopalrao@gmail.com>
2023-08-24 22:15:59 +1000

    update backtracking

--- src/backtracking/Permutations.java
+++ src/backtracking/Permutations.java
@@ -4,29 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Permutations {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int[] nums = new int[]{1, 2, 3, 4};
+		int[] nums = new int[]{1, 2, 3, 4};
 		permute(nums);
 	}
 	
 	public static List<List<Integer>> permute(int[] nums) {
-		   List<List<Integer>> list = new ArrayList<>();
-		   backtrack(list, new ArrayList<>(), nums);
-		   return list;
+		List<List<Integer>> list = new ArrayList<>();
+		backtrack(list, new ArrayList<>(), nums);
+		return list;
+	}
+	
+	private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+		if(tempList.size() == nums.length){
+			list.add(new ArrayList<>(tempList));
+		} else{
+			for(int i = 0; i < nums.length; i++){ 
+				if(tempList.contains(nums[i])) continue; // element already exists, skip
+				tempList.add(nums[i]);
+				backtrack(list, tempList, nums);
+				tempList.remove(tempList.size() - 1);
+			}
 		}
-
-		private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
-		   if(tempList.size() == nums.length){
-		      list.add(new ArrayList<>(tempList));
-		   } else{
-		      for(int i = 0; i < nums.length; i++){ 
-		         if(tempList.contains(nums[i])) continue; // element already exists, skip
-		         tempList.add(nums[i]);
-		         backtrack(list, tempList, nums);
-		         tempList.remove(tempList.size() - 1);
-		      }
-		   }
-		} 
+	} 
 }
