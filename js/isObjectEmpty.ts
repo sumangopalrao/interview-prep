cc0033fcb968f647aa3cd6d4fb4871f45ff81ff5
Suman Rao <suman.gopalrao@gmail.com>
2023-08-26 23:38:40 +1000

    Find if object is empty

---
+++ js/isObjectEmpty.ts
@@ -0,0 +1,9 @@
+// Given an array or object, find out if it is empty?
+// https://leetcode.com/problems/is-object-empty/description/
+
+function isEmpty(obj: Record<string, any> | any[]): boolean {
+    if ((Array.isArray(obj) && !obj.length) || JSON.stringify(obj) === '{}') {
+        return true;
+    }
+    return false;
+};
