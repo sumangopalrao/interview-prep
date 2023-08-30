f170ad8c91fb8a659631f2cdebf635a8a33e504f
Suman Gopal Rao <suman.gopalrao@gmail.com>
2023-08-30 22:52:42 +1000

    Add last elment of array

---
+++ js/last-element-of-array.js
@@ -0,0 +1,12 @@
+// Add function to print the last element of an array
+// https://leetcode.com/problems/array-prototype-last/description/
+
+Array.prototype.last = function() {
+    if (this.length === 0) {
+        return -1;
+    }
+    return this[this.length - 1];
+};
+
+const arr = [1, 2, 3];
+arr.last(); // 3
