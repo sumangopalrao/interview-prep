1d2a753a2ff0aa094ff7b49db4bd612030adb8b0
Suman Gopal Rao <suman.gopalrao@gmail.com>
2023-09-05 18:29:09 +1000

    Reverse between nodes

--- src/linkedlist/ReverseBetweenNodes.java
+++ src/linkedlist/ReverseBetweenNodes.java
@@ -4,20 +4,28 @@ public class ReverseBetweenNodes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i=0;
-        Node curr = Node.initializeSample();
+        Node head = Node.initializeSample();
         int m = 2;
         int n = 4;
+        Node reversedHead = reverse(head, m, n);
+        Node.printList(reversedHead);
+    }
+
+
+    static Node reverse(Node head, int m, int n) {
+        int i = 1;
         Node prev = null;
-        while(curr != null && i<m-1) {
+        Node curr = head;
+        while(curr != null && i<m) {
             prev = curr;
             curr = curr.link;
             i++;
         }
-        System.out.println("The value of i:"+i);
+        // System.out.println("The value of i:"+i);
         Node temp_prev = prev;
         Node temp_curr = curr;
         Node next = null;
+        prev = null;
         while(i<=n && curr != null) {
             next = curr.link;
             curr.link = prev;
@@ -25,11 +33,40 @@ public class ReverseBetweenNodes {
             curr = next;
             i++;
         }
-        temp_prev.link = next;
-        next.link = temp_curr;
-     //   System.out.println("The prev and curr values:"+prev.val+","+curr.val);
-        System.out.println("Head value:"+curr.data);
-        //   return head;
+        if (temp_prev != null) {
+            temp_prev.link = prev; // 'prev' now points to the start of the reversed segment
+        } else {
+            head = prev; // if 'm' is 1, then update the head of the list
+        }
+        temp_curr.link = curr; // 'temp_curr' now points to the end of the reversed segment
+    
+        return head;
 	}
+
+    // static Node reverse(Node head, int m, int n) {
+    //     if (head == null) return null;
+    
+    //     Node dummy = new Node(0); // This dummy node will help us in case m is 1
+    //     dummy.link = head;
+    //     Node prev = dummy;
+    
+    //     // Traverse the list till position m
+    //     for (int i = 0; i < m - 1; i++) {
+    //         prev = prev.link;
+    //     }
+    
+    //     Node curr = prev.link;
+    //     Node next = null;
+    
+    //     // Keep reversing the nodes till position n
+    //     for (int i = 0; i < n - m + 1; i++) {
+    //         next = curr.link;
+    //         curr.link = next.link;
+    //         next.link = prev.link;
+    //         prev.link = next;
+    //     }
+    
+    //     return dummy.link; // If m was 1, the start would have changed, hence return dummy.link
+    // }
 
 }
