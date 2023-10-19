60c9827dad9d9ebcf8eed9968784aebe950a9b28
Suman Rao <suman.gopalrao@gmail.com>
2023-10-19 22:22:24 +1100

    Update subsequene problem

--- src/arrays/IsSubsequence.java
+++ src/arrays/IsSubsequence.java
@@ -13,7 +13,6 @@ public class IsSubsequence {
     }
 
     private static boolean isSubsequence(String s, String t) {
-
         int j = 0;
         for (int i=0; i<s.length(); i++) {
             while (j < t.length() && t.charAt(j) != s.charAt(i)) {
@@ -27,7 +26,6 @@ public class IsSubsequence {
             if (t.charAt(j) == s.charAt(i)) {
                 j++;
             }
-
         }
         return true;
     }
