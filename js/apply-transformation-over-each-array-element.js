7ee011ee6a7977f6fd5ad56611f4a221677a843e
Suman Rao <suman.gopalrao@gmail.com>
2023-11-28 22:16:16 +1100

    Add apply transformation

---
+++ js/apply-transformation-over-each-array-element.js
@@ -0,0 +1,14 @@
+// WAP that implements a map kinda function on an array
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const result = [];
+    for (let index = 0; index < arr.length; index++) {
+        result.push(fn(arr[index], index));
+    }
+    return result;
+};
