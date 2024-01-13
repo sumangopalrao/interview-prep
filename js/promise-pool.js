0764b85381ec4d3d6f06d509a87bcb2ec1695865
Suman Gopal Rao <suman.gopalrao@gmail.com>
2024-01-13 16:15:11 +1100

    Add promises pool

--- js/promise-pool.js
+++ js/promise-pool.js
@@ -2,33 +2,41 @@
 // https://leetcode.com/problems/promise-pool
 
 /**
- * @param {Function[]} functions
- * @param {number} n
- * @return {Promise<any>}
- */
+* @param {Function[]} functions
+* @param {number} n
+* @return {Promise<any>}
+*/
 var promisePool = async function(functions, n) {
-
-    const results = [];
-    let i = 0;
-    console.log('Functions are', functions);
-    const isPromisePendin = false;
-    while(i<n) {
-        console.log('Function is', functions[i]);
-        const exectutedValue = functions[i]();
-        console.log("executed value is", exectutedValue);
-        functions[i]().then((res) => {
-            console.log("result is", res);
-            results.push(res);
-        });
-        i++;
-    }
-
-    return new Promise((res, err) => {
-        res(results);
-    });
     
-};
-
+    const results = [];
+    
+    return new Promise((res, err) => {
+        let i = 0;
+        let numberOfPromisesPending = 0;
+        
+        console.log('Functions are', functions);
+        
+        function cb() {
+            if (i === functions.length && numberOfPromisesPending === 0) {
+                res();
+            }
+            while(i< functions.length && numberOfPromisesPending < n) {
+                // console.log('Function is', functions[i]);
+                // const exectutedValue = functions[i]();
+                // console.log("executed value is", exectutedValue);
+                functions[i]().then(() => {
+                    // console.log("result is", res);
+                    // results.push(res);
+                    numberOfPromisesPending--;
+                    cb();
+                });
+                i++;
+                numberOfPromisesPending++;
+            }
+        }
+        cb();
+    });
+}
 
 const sleep = (t) => new Promise(res => setTimeout(res, t));
 promisePool([() => sleep(500), () => sleep(400)], 1)
