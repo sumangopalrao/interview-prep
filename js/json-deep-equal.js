2947efc40c1251655e9b4311d8d5791209ec75b4
Suman Gopal Rao <suman.gopalrao@gmail.com>
2024-01-16 22:18:59 +1100

    add json deep equal

--- js/json-deep-equal.js
+++ js/json-deep-equal.js
@@ -1,16 +1,75 @@
+// Program to replicate implementation of JSON Deep Equal
+// https://leetcode.com/problems/json-deep-equal
+
 /**
- * @param {null|boolean|number|string|Array|Object} o1
- * @param {null|boolean|number|string|Array|Object} o2
- * @return {boolean}
- */
-var areDeeplyEqual = function(o1, o2) {
-    if(typeof o1 !== 'object' && typeof o2 !== 'object') {
+* @param {null|boolean|number|string|Array|Object} o1
+* @param {null|boolean|number|string|Array|Object} o2
+* @return {boolean}
+*/
+var areDeeplyEqual = function (o1, o2) {
+    if (o1 === null && o2 === null)
+        return true;
+    if (o1 === null && o2 !== null)
+        return false;
+    if (o2 === null && o1 !== null)
+        return false;
+    if (typeof o1 === 'boolean' && typeof o2 === 'boolean')
+        return o1 === o2;
+    if (!o1 && !o2)
+        return true;
+    if (!o1)
+        return false;
+    if (!o2)
+        return true
+    if (typeof o1 !== 'object' && typeof o2 !== 'object') {
+        // console.log('Inside if condition');
         return o1 === o2;
     }
-    if (o1.length !== )
-    return false;
+
+
+    function isChildrenEqual(o1, o2) {
+        const keys = Object.keys(o1);
+        // console.log('Keys are', keys);
+        if (Object.keys(o1).length !== Object.keys(o2).length)
+            return false;
+        for (let key of keys) {
+            // console.log('Key is', key);
+            if (Object.hasOwn(o2, key)) {
+                // isValid = true;
+                if(!areDeeplyEqual(o1[key], o2[key]))
+                // console.log('Is valid', isValid);
+                // if (!isValid)
+                    return false;
+            }
+            else {
+                // isValid = false;
+                return false;
+            }
+        };
+        return true;
+    }
+
+    if (Array.isArray(o1) && Array.isArray(o2)) {
+        if (o1.length !== o2.length) {
+            return false;
+        }
+        return isChildrenEqual(o1, o2);
+    }
+
+    if ((Array.isArray(o1) && !Array.isArray(o2)) || (Array.isArray(o2) && !Array.isArray(o1))) {
+        return false;
+    }
+
+    return isChildrenEqual(o1, o2); 
 };
 
-console.log('Boolean is', areDeeplyEqual(1, 2));
-console.log(typeof [1, 2, 3]);
-console.log(typeof { 'key': 1, 'val': 3});
+// console.log('Boolean is', areDeeplyEqual(1, 2));
+// console.log(typeof [1, 2, 3]);
+// console.log(typeof { 'key': 1, 'val': 3});
+console.log(areDeeplyEqual({ key: 'value' }, { key: 'value' }));
+console.log(areDeeplyEqual({ key: 'value123' }, { key: 'value' }));
+console.log(areDeeplyEqual(['value123', 'value'], ['value123', 'value123231']));
+console.log(areDeeplyEqual(true, false));
+console.log(areDeeplyEqual([1], { "0": 1, "length": 1 }));
+console.log(areDeeplyEqual({ "0": 1 }, [1]));
+console.log(areDeeplyEqual({}, null));
