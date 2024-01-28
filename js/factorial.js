296be98ecb9753a2cb2ed6c8dc5704d71a13c7d4
Suman Rao <suman.gopalrao@gmail.com>
2024-01-28 22:41:32 +1100

    Factorial

---
+++ js/factorial.js
@@ -0,0 +1,19 @@
+// WAP to return the factorial of a number
+// https://leetcode.com/problems/factorial-generator/description/
+
+function* factorial(n) {
+    if (n == 0)
+        yield 1;
+
+    let fact = 1;
+    for (let i=1; i<=n; i++) {
+        fact *= i;
+        yield fact;
+    }
+}
+
+const f = factorial(3);
+ 
+console.log(f.next().value);
+// console.log(f.next().value);
+// console.log(f.next().value);
