91cdf3ed0e9e805695844870bd10cb56da74e88b
Suman Gopal Rao <suman.gopalrao@gmail.com>
2024-06-26 19:46:49 +1000

    Add max ascending sub array

---
+++ ts/maxAscendingSubArray.ts
@@ -0,0 +1,31 @@
+// Problem: Find maximum ascending subarray
+// https://leetcode.com/problems/maximum-ascending-subarray-sum/description/
+// Runtime: O(n)
+
+
+function maxAscendingSubArray(nums: number[]): number{
+    let maxSumSoFar = 0;
+
+    if (nums.length === 1) 
+        return nums[0];
+    let currentSum = nums[0];
+    maxSumSoFar = currentSum;
+    for(let i=1; i<nums.length; i++) {
+        if (nums[i] > nums[i-1]) {
+            currentSum += nums[i];
+        } else {
+            currentSum = nums[i];
+        }
+
+        if (currentSum > maxSumSoFar)
+            maxSumSoFar = currentSum;
+    }
+
+    return maxSumSoFar;
+};
+
+console.log(maxAscendingSubArray([10,20,30,5,10,50]));
+console.log(maxAscendingSubArray([10,20,30,40,50]));
+console.log(maxAscendingSubArray([12,17,15,13,10,11,12]));
+console.log(maxAscendingSubArray([1]));
+console.log(maxAscendingSubArray([100,10,1]));
