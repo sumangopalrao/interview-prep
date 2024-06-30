cc54ea8d10f18bc20d2f4208fa36060348925b83
Suman Gopal Rao <suman.gopalrao@gmail.com>
2024-06-30 19:34:31 +1000

    Update number of days in month

--- src/math/NumberOfDaysInMonth.java
+++ src/math/NumberOfDaysInMonth.java
@@ -1,10 +1,10 @@
 // Given a year and month, return the number of days in that month.
 //https://leetcode.com/problems/number-of-days-in-a-month/description/ 
 
-package Math;
+package math;
 
-public Solution {
-  public int numberOfDays(int year, int month) {
+public class NumberOfDaysInMonth {
+  public static int numberOfDays(int year, int month) {
         if (month == 9 || month == 4 || month == 6 || month == 11) {
             return 30;
         }
@@ -24,4 +24,10 @@ public Solution {
         return 31;
         
     }
+
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(numberOfDays(1992, 7));
+	}
 }
