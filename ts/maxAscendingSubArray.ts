a7b104883da4dd5ecd7e415d9cc35dce8516983d
Suman <suman.gopalrao@gmail.com>
2024-07-05 23:46:37 +1000

    Update maxAscendingSubArray.ts

--- ts/maxAscendingSubArray.ts
+++ ts/maxAscendingSubArray.ts
@@ -2,14 +2,15 @@
 // https://leetcode.com/problems/maximum-ascending-subarray-sum/description/
 // Runtime: O(n)
 
-
 function maxAscendingSubArray(nums: number[]): number{
     let maxSumSoFar = 0;
 
     if (nums.length === 1) 
         return nums[0];
+    
     let currentSum = nums[0];
     maxSumSoFar = currentSum;
+    
     for(let i=1; i<nums.length; i++) {
         if (nums[i] > nums[i-1]) {
             currentSum += nums[i];
